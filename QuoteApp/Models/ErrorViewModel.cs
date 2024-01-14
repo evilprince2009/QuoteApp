7dd9da3bcd73c7e46ea3811c7018723c0a14e07e
Ibne Nahian <evilprince2009@users.noreply.github.com>
2024-01-14 20:04:24 +0600

    Initial Commit

---
+++ QuoteApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace QuoteApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
