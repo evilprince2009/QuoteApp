600c1b5f85bd87eedacb1634375ecf5f72ae82aa
Ibne Nahian <evilprince2009@users.noreply.github.com>
2024-01-14 20:07:20 +0600

    Create Quote.cs

---
+++ QuoteApp/Models/Quote.cs
@@ -0,0 +1,15 @@
+﻿namespace QuoteApp.Models
+{
+    public class Quote
+    {
+        public int Id { get; set; }
+        public required string Quotation { get; set; }
+        public required string Author { get; set; }
+        public required string Genre { get; set; }
+
+        public Quote()
+        {
+
+        }
+    }
+}
