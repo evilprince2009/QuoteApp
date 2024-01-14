c4366aee64b578372a480489030470f7a3d9b12e
Ibne Nahian <evilprince2009@users.noreply.github.com>
2024-01-14 21:12:23 +0600

    cleanup

--- QuoteApp/Program.cs
+++ QuoteApp/Program.cs
@@ -29,8 +29,7 @@ namespace QuoteApp
             }
             else
             {
-                app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseExceptionHandler("/Shared/Error");
                 app.UseHsts();
             }
 
